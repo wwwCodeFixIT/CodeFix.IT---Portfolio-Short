71c7c456e3207356e02e0c2294aff3639bc99863
wwwCodeFixIT <wwwcodefixit@gmail.com>
2026-09-26 16:12:17 +0200

    Route project case studies by stable slug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { HomepageV1 } from './pages/HomepageV1';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
+import { ProjectCaseStudy } from './pages/ProjectCaseStudy';
+import { projects } from './data/projects';
 
 function App() {
   const path = window.location.pathname.replace(/\/+$/, '') || '/';
 
   if (path === '/polityka-prywatnosci') {
     return <PrivacyPolicy />;
+  }
+
+  if (path.startsWith('/realizacje/')) {
+    const slug = path.replace('/realizacje/', '');
+    const project = projects.find((item) => item.slug === slug);
+    if (project) return <ProjectCaseStudy project={project} />;
   }
 
   return <HomepageV1 />;
